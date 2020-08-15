e6240bfcf3f49f48c77ee173778a96ba891a7381
Brad <culleyib@gmail.com>
2020-08-14 21:50:32 -0500

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Switch, Route, Redirect, BrowserRouter} from 'react-router-dom';
+import Page1 from './Page1';
+import Page2 from './Page2';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/1" component={Page1}/>
+          <Route exact path="/2" component={Page2}/>
+          <Redirect to="/1"/>
+        </Switch>
+      </BrowserRouter>
   );
 }
 
